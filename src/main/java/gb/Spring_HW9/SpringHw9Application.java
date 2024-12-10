ac33f081c4a3841778deab6b16b0bd915407a183
MosolovIR <mosolovolosom@gmail.com>
2024-12-10 18:41:52 +0300

    Initial commit

---
+++ src/main/java/gb/Spring_HW9/SpringHw9Application.java
@@ -0,0 +1,13 @@
+package gb.Spring_HW9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHw9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHw9Application.class, args);
+	}
+
+}
---
+++ src/test/java/gb/Spring_HW9/SpringHw9ApplicationTests.java
@@ -0,0 +1,13 @@
+package gb.Spring_HW9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
